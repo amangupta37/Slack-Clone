681a6c8460b614bf64264457f08b37dbaabe7a98
amangupta37 <amangupta6121998@gmail.com>
2021-03-03 19:55:12 +0530

    added a firebase file

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCzB8ge-wjs4XGu_CbUT7aJuRPG1ODyi0M",
+    authDomain: "slack-clone-21b04.firebaseapp.com",
+    projectId: "slack-clone-21b04",
+    storageBucket: "slack-clone-21b04.appspot.com",
+    messagingSenderId: "493993507889",
+    appId: "1:493993507889:web:941a03bdb4bb0808512910"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+
+
+  export default db;
+  
