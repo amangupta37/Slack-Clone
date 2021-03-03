4ac31e649c63fb055afdd29151d1282dc0a4be13
amangupta37 <amangupta6121998@gmail.com>
2021-03-03 19:53:09 +0530

    upadated App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import './App.css';
+import {useEffect, useState} from 'react';
 import{BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Chat from './components/Chat';
 import Login from './components/Login';
 import styled from 'styled-components';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import db from './firebase';
+
 
 function App() {
+
+
+   const [rooms, setstate] = useState([]);
+
+  const getChannels = () =>{
+
+    db.collection('rooms').onSnapshot((snapshot) =>{
+
+     setstate(snapshot.docs.map((doc) => {
+       return {id: doc.id , name : doc.data().name};
+     })
+     
+     )
+
+    })
+
+  }
+useEffect(() => {
+  getChannels();
+}, [])
+ 
+console.log(rooms);
+
+
   return (
     <div className="App">
       <Router>
           <Container>
               <Header />
                 <Main>
-                  <Sidebar />
+                  <Sidebar room ={rooms} />
                   <Switch>
                        <Route path="/room">
                            <Chat />
